250ba8a323c1fbacbc8ad9ee5ffe8077bf3e99d9
Mykhailok <kosinskiy.mihael@gmail.com>
2016-08-27 00:41:27 +0300

    HW2_3 finish

--- src/module2/homework/HW2_3.java
+++ src/module2/homework/HW2_3.java
@@ -3,38 +3,27 @@ package module2.homework;
 public class HW2_3 {
     private static double commission = 0.05;
     private static String nameOwn = "Ann";
-    private static double withdrawal = 100;
+    private static double withdrawal = 150;
     private static int[] balances = {1200, 250, 2000, 500, 3200};
     private static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
 
 
 
 
-    static String[] getOwnerNames(int[] balances, String[] ownerNames) {
+    static double getOwnerNames(int[] balances, String[] ownerNames) {
+        double rest = 0;
 
-        for (int balance : balances) {
-            double sum = balance - withdrawal*commission;
-
-
-
-            System.out.println(sum);
-        }
-
-
-
-        for(String name : ownerNames) {
-            if (name.equals(nameOwn)) {
-
-                System.out.println(name);
+        for (int i = 0; i < balances.length; i++){
+           double balance = balances[i];
+            rest = balance - withdrawal - withdrawal*commission;
+            if (rest > 0) {
+                if (nameOwn.equals(ownerNames[i])) {
+                    System.out.println(ownerNames[i] + " " + withdrawal + " " + rest);
+                }
             }
-        }
-
-
-
-        return ownerNames;
-
-
-
+            else System.out.println(ownerNames[i] + " NO");
+         }
+        return rest;
     }
     public static void main(String[] args) {
             getOwnerNames(balances,ownerNames);
