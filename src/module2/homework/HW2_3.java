7676000fe01db406c08a9ef3fec08e7e441b28c6
Mykhailok <kosinskiy.mihael@gmail.com>
2016-08-26 18:12:22 +0300

    HW2_3 not finished

---
+++ src/module2/homework/HW2_3.java
@@ -0,0 +1,41 @@
+package module2.homework;
+
+public class HW2_3 {
+    private static double commission = 0.05;
+    private static String nameOwn = "Ann";
+    private static double withdrawal = 100;
+    private static int[] balances = {1200, 250, 2000, 500, 3200};
+    private static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+
+    static String[] getOwnerNames(int[] balances, String[] ownerNames) {
+
+        for (int balance : balances) {
+            double sum = balance*commission;
+
+
+
+            System.out.println(balance+" "+sum);
+        }
+
+
+
+        for(String name : ownerNames) {
+            if (name == nameOwn) {
+
+                System.out.println(name);
+            }
+        }
+
+
+
+        return ownerNames;
+
+
+
+    }
+    public static void main(String[] args) {
+            getOwnerNames(balances,ownerNames);
+    }
+
+}
