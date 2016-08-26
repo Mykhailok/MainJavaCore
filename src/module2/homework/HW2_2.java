86a5f2e0b8ba79accbdf23f09c3d6a79fb10df29
Mykhailok <kosinskiy.mihael@gmail.com>
2016-08-26 16:26:02 +0300

    HW 2_2 Ready

--- src/module2/homework/HW2_2.java
+++ src/module2/homework/HW2_2.java
@@ -1,22 +1,23 @@
 package module2.homework;
 
 public class HW2_2 {
+    private static double commission;
 
     static double withdrawBalance(double balance, double withdrawal) {
-        double commission = withdrawal*0.05;
-            System.out.println(commission);
+        commission = withdrawal*0.05;
         return balance - withdrawal - commission;
+
     }
 
     public static void main(String[] args) {
 
-        double balanceAfter = withdrawBalance(100, 99);
+        double balanceAfter = withdrawBalance(100, 50);
 
         if (balanceAfter >= 0) {
-            System.out.println("OK "+ balanceAfter);
+            System.out.println("OK "+ commission + " " + balanceAfter);
         }
         else{
-            System.out.println("NO");
+            System.out.println(" NO");
         }
     }
 
