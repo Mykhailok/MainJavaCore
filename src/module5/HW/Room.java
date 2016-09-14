e51a51f98ba6f231c9bfc18c48391c9b83670f68
Mykhailok <kosinskiy.mihael@gmail.com>
2016-09-14 16:14:10 +0300

    homework 5.1 + UML

---
+++ src/module5/HW/Room.java
@@ -0,0 +1,96 @@
+package module5.HW;
+
+import java.util.Date;
+
+public class Room {
+    private long id;
+    private int price;
+    private int persons;
+    private Date dateAvailableFrom;
+    private String hotelName;
+    private String cityName;
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        Room room = (Room) object;
+
+        if (id != room.id) return false;
+        if (price != room.price) return false;
+        return cityName.equals(room.cityName);
+
+        //null.methods - as a result nullPointerException
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + price;
+        result = 31 * result + (cityName != null ? cityName.hashCode() : 0);
+        return result;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getPersons() {
+        return persons;
+    }
+
+    public void setPersons(int persons) {
+        this.persons = persons;
+    }
+
+    public Date getDateAvailableFrom() {
+        return dateAvailableFrom;
+    }
+
+    public void setDateAvailableFrom(Date dateAvailableFrom) {
+        this.dateAvailableFrom = dateAvailableFrom;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+   //Constructor
+
+
+    public Room(long id, int price, int persons, Date dateAvailableFrom, String hotelName, String cityName) {
+        this.id = id;
+        this.price = price;
+        this.persons = persons;
+        this.dateAvailableFrom = dateAvailableFrom;
+        this.hotelName = hotelName;
+        this.cityName = cityName;
+    }
+}
