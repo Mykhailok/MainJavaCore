db50f527cc22f49dac144e634ae7823de6b3d859
Mykhailok <kosinskiy.mihael@gmail.com>
2016-09-15 13:26:03 +0300

    homework 5.4

--- src/module5/HW/GoogleAPI.java
+++ src/module5/HW/GoogleAPI.java
@@ -5,15 +5,16 @@ public class GoogleAPI implements API {
     private Room[] rooms = new Room[5];
 
     public GoogleAPI() {
-        Room room1 = new Room(1, 500, 2, API.getDateWithShift(1), "Hayat", "Kyiv");
+
+        Room room1 = new Room(1, 500, 2, API.getDateWithShift(ONE_DAY_SHIFT), "Hayat", "Kyiv");
         rooms[0] = room1;
-        Room room2 = new Room(2, 1000, 1, API.getDateWithShift(1), "Lybid", "Kyiv");
+        Room room2 = new Room(2, 1000, 3, API.getDateWithShift(ONE_DAY_SHIFT), "Lybid", "Kyiv");
         rooms[1] = room2;
-        Room room3 = new Room(3, 1700, 2, API.getDateWithShift(1), "Hayat", "Kyiv");
+        Room room3 = new Room(3, 1700, 2, API.getDateWithShift(ONE_DAY_SHIFT), "Hayat", "Kyiv");
         rooms[2] = room3;
-        Room room4 = new Room(4, 2200, 1, API.getDateWithShift(1), "Hayat", "Kyiv");
+        Room room4 = new Room(4, 2200, 1, API.getDateWithShift(ONE_DAY_SHIFT), "Hayat", "Kyiv");
         rooms[3] = room4;
-        Room room5 = new Room(5, 2500, 3, API.getDateWithShift(1), "Odessa", "Odessa");
+        Room room5 = new Room(5, 2500, 4, API.getDateWithShift(ONE_DAY_SHIFT), "Odessa", "Odessa");
         rooms[4] = room5;
 
     }
@@ -27,4 +28,6 @@ public class GoogleAPI implements API {
     public Room[] getAll() {
         return new Room[0];
     }
+
+
 }
--- src/module5/HW/TripAdvisorAPI.java
+++ src/module5/HW/TripAdvisorAPI.java
@@ -1,30 +1,31 @@
 package module5.HW;
 
-public class TripAdvisorAPI implements API{
+public class TripAdvisorAPI implements API {
 
     private Room[] rooms = new Room[5];
 
     public TripAdvisorAPI() {
-        Room room1 = new Room(1, 500, 2, API.getDateWithShift(1), "Hayat", "Kyiv");
+
+        Room room1 = new Room(1, 500, 1, API.getDateWithShift(ONE_DAY_SHIFT), "Hayat", "Kyiv");
         rooms[0] = room1;
-        Room room2 = new Room(2, 1000, 1, API.getDateWithShift(1), "Lybid", "Kyiv");
+        Room room2 = new Room(2, 1000, 1, API.getDateWithShift(ONE_DAY_SHIFT), "Lybid", "Kyiv");
         rooms[1] = room2;
-        Room room3 = new Room(3, 1600, 2, API.getDateWithShift(1), "Hayat", "Kyiv");
+        Room room3 = new Room(3, 1600, 4, API.getDateWithShift(ONE_DAY_SHIFT), "Hayat", "Kyiv");
         rooms[2] = room3;
-        Room room4 = new Room(4, 2100, 1, API.getDateWithShift(1), "Hayat", "Kyiv");
+        Room room4 = new Room(4, 2100, 1, API.getDateWithShift(ONE_DAY_SHIFT), "Hayat", "Kyiv");
         rooms[3] = room4;
-        Room room5 = new Room(5, 2500, 3, API.getDateWithShift(1), "Odessa", "Odessa");
+        Room room5 = new Room(5, 2500, 2, API.getDateWithShift(ONE_DAY_SHIFT), "Odessa", "Odessa");
         rooms[4] = room5;
 
     }
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-        return new Room[0];
+        return new Room[5];
     }
 
     @Override
     public Room[] getAll() {
-        return new Room[0];
+        return new Room[5];
     }
 }
