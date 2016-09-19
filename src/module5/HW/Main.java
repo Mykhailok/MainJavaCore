faf937be805d36de77ca8a9624aac44b5db5ba87
Mykhailok <kosinskiy.mihael@gmail.com>
2016-09-19 18:45:09 +0300

    code review main2

--- src/module5/HW/Main.java
+++ src/module5/HW/Main.java
@@ -2,44 +2,33 @@ package module5.HW;
 
 public class Main {
     public static void main(String[] args) {
-        //create apis array
-        //BookingComAPI bookingComAPI = new BookingComAPI();
-        //bookingComAPI.findRooms(500,2,"Hayat", "Kyiv");
-        //bookingComAPI.findRooms(0,0,null, null);
-        //System.out.println("All good " + bookingComAPI);
+
         Controller controller = new Controller();
 
+        System.out.println("Finding request rooms...");
         controller.requestRooms(500,2,"Hayat", "Kyiv");
         controller.requestRooms(1600,4,"Hayat", "Kyiv");
         controller.requestRooms(2500,2,"Hayat", "Kyiv");
         System.out.println(controller.toString());
 
-
+        System.out.println("Checking APIs...");
         Room[] res =  controller.check(new BookingComAPI(),new GoogleAPI());
         if (res.length != 0){
-            System.out.println(res.length);
+            System.out.println("Found "+res.length + " room(s)");
             System.out.println(controller.toString());
         }
-
-
 
         Room[] res2 =  controller.check(new BookingComAPI(),new TripAdvisorAPI());
         if (res2.length != 0){
-            System.out.println(res2.length);
+            System.out.println("Found "+res2.length + " room(s)");
             System.out.println(controller.toString());
         }
-
 
         Room[] res3 =  controller.check(new GoogleAPI(),new TripAdvisorAPI());
         if (res3.length != 0){
-            System.out.println(res3.length);
+            System.out.println("Found "+res3.length + " room(s)");
             System.out.println(controller.toString());
         }
-
-
-
-
-
 
     }
 }
