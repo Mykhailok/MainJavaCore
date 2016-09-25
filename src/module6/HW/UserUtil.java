aea7e6e8f1a5ceae95a8b951e47223ea71daf986
Mykhailok <kosinskiy.mihael@gmail.com>
2016-09-25 23:33:20 +0300

    homework 6.5 finish

--- src/module6/HW/UserUtil.java
+++ src/module6/HW/UserUtil.java
@@ -2,5 +2,25 @@ package module6.HW;
 
 public class UserUtil {
 
+    public User[] uniqueUsers(User[] users) {
+        return new User[0];
+    }
+
+    public User[] usersWithContitionalBalance(User[] users, int balance) {
+        return new User[0];
+    }
+
+    public static final User[] paySalaryToUsers(User[] users) {
+        return null;
+    }
+
+    public static final long[] getUsersId(User[] users) {
+        return null;
+    }
+
+    public User[] deleteEmptyUsers(User[] users) {
+        return new User[0];
+    }
+
 
 }
