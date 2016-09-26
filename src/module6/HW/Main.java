49b8168bacd0153dc32c26f6ee64ad9409149f36
Mykhailok <kosinskiy.mihael@gmail.com>
2016-09-26 20:44:54 +0300

    homework 6.5 unique Users

--- src/module6/HW/Main.java
+++ src/module6/HW/Main.java
@@ -5,11 +5,32 @@ import java.util.Arrays;
 public class Main {
     static int[] array = {1, 2, 3, 4};
 
-    public static void main(String[] args) {
+    static User[] users = new User[5];
 
+
+
+    public Main(){
+        User user1 = new User(1001, "FirstName", "LastName", 1000, 1000);
+        users[0] = user1;
+        User user2 = new User(1002, "FirstName2", "LastName2", 2000, 2000);
+        users[1] = user2;
+        User user3 = new User(1003, "FirstName3", "LastName3", 3000, 3000);
+        users[2] = user3;
+        User user4 = new User(1004, "FirstName4", "LastName4", 4000, 4000);
+        users[3] = user4;
+        User user5 = new User(1005, "FirstName5", "LastName5", 5000, 5000);
+        users[4] = user5;
+    }
+
+
+
+    public static void main(String[] args) {
+        new Main();
         System.out.println("Исходный массив: " + Arrays.toString(array));
         System.out.println("Перевернутый массив: " + Arrays.toString(ArrayUtils.reverse(array)));
         System.out.println("Четные элементы массива: " + Arrays.toString(ArrayUtils.findEvenElements(array)));
+        System.out.println(Arrays.toString(UserUtil.uniqueUsers(users)));
+
     }
 
 }
--- src/module6/HW/UserUtil.java
+++ src/module6/HW/UserUtil.java
@@ -2,8 +2,15 @@ package module6.HW;
 
 public class UserUtil {
 
-    public User[] uniqueUsers(User[] users) {
-        return new User[0];
+    public static User[] uniqueUsers(User[] users) {
+        User[] result = new User[users.length];
+        for (int i = 0; i < users.length; i++) {
+            if (users[i+1].equals(users[i])){
+                result[i] = users[i];
+            }
+        }
+
+        return result;
     }
 
     public User[] usersWithContitionalBalance(User[] users, int balance) {
