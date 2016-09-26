4d01e9a848e91cd2fb9f14d60c400648343e1576
Mykhailok <kosinskiy.mihael@gmail.com>
2016-09-27 01:20:14 +0300

    homework 6.5 unique finish

--- src/module6/HW/UserUtil.java
+++ src/module6/HW/UserUtil.java
@@ -7,10 +7,20 @@ import java.util.Arrays;
 public class UserUtil {
 
     public static User[] uniqueUsers(User[] users) {
-        User[] result = new User[users.length];
+        ArrayList result = new ArrayList();
+        for(int i=0; i < users.length; i++) {
+            for (int j = 0; j < users.length; j++) {
+                if (users[i] != null && i != j && (users[i].equals(users[j])))
+                        users[j] = null;
+            }
+        }
+            for (User i : users) {
+                if (i != null){
+                    result.add(i);
+                }
+            }
 
-
-        return result;
+        return (User[]) result.toArray(new User[result.size()]);
     }
 
     public User[] usersWithContitionalBalance(User[] users, int balance) {
