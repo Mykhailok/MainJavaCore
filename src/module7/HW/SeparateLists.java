7ba18cc76b1cf9e1bb70515fe2b912cf79f2982e
Mykhailok <kosinskiy.mihael@gmail.com>
2016-10-13 14:24:36 +0300

    separateList class finish

--- src/module7/HW/SeparateLists.java
+++ src/module7/HW/SeparateLists.java
@@ -1,17 +1,14 @@
 package module7.HW;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class SeparateLists {
 
     public static void main(String[] args) {
         List<Order> makeOrderList = new ArrayList<>();
-
         List<List<Order>> orderLists = new ArrayList<List<Order>>();
-        List<String> cities = new ArrayList<>();
 
+        Set<String> cities = new HashSet<>();
 
         makeOrderList.add(new Order(1,100,Currency.UAH,"ItemName1","Shop1", new User(10,"FirstName1", "LastName1", "Kyiv", 1001)));
         makeOrderList.add(new Order(2,200,Currency.UAH,"ItemName2","Shop2", new User(20,"FirstName1", "LastName1", "Kyiv", 2001)));
@@ -19,14 +16,19 @@ public class SeparateLists {
         makeOrderList.add(new Order(4,400,Currency.UAH,"ItemName4","Shop4", new User(40,"FirstName4", "LastName4", "Lviv", 4001)));
 
         for (Order getCity : makeOrderList){
-            if (!cities.contains(getCity.getUser().getCity())){
+            if (getCity.getUser().getCity() != null){
                 cities.add(getCity.getUser().getCity());
-                orderLists.add(new ArrayList<>());
             }
-
         }
-
-        //orderLists.add(new ArrayList<>(Arrays.asList(separateCity)));
+            for (String s : cities){
+                List<Order> temp = new ArrayList<>();
+                for (int i = 0; i < makeOrderList.size(); i++){
+                   if (makeOrderList.get(i).getUser().getCity() == s){
+                       temp.add(makeOrderList.get(i));
+                   }
+                }
+                orderLists.add(temp);
+            }
 
         System.out.println(cities);
         System.out.println(orderLists);
