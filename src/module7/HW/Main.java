9f8cd146867abbad4d67090634cbc32961ee2c55
Mykhailok <kosinskiy.mihael@gmail.com>
2016-10-14 10:41:30 +0300

    change main at HW7.2

--- src/module7/HW/Main.java
+++ src/module7/HW/Main.java
@@ -112,7 +112,7 @@ public class Main {
         System.out.println("Result:");
         System.out.println(result);*/
 
-        List<List<User>> uniqeCity = new ArrayList<>();
+        /*List<List<User>> uniqeCity = new ArrayList<>();
         List<User> usersCity = new ArrayList<>();
         List<User> usersCity2 = new ArrayList<>();
         List<String> cities = new ArrayList<>();
@@ -145,7 +145,35 @@ public class Main {
             }
         }
         //System.out.println(cities);
-        System.out.println("as many lists as many unique cities are in User: "+resultList);
+        System.out.println("as many lists as many unique cities are in User: "+resultList);*/
+
+        List<Order> makeOrderList = new ArrayList<>();
+        List<List<Order>> orderLists = new ArrayList<List<Order>>();
+
+        Set<String> cities = new HashSet<>();
+
+        makeOrderList.add(new Order(1,100,Currency.UAH,"ItemName1","Shop1", new User(10,"FirstName1", "LastName1", "Kyiv", 1001)));
+        makeOrderList.add(new Order(2,200,Currency.UAH,"ItemName2","Shop2", new User(20,"FirstName1", "LastName1", "Kyiv", 2001)));
+        makeOrderList.add(new Order(3,300,Currency.UAH,"ItemName3","Shop3", new User(30,"FirstName1", "LastName1", "Odessa", 3001)));
+        makeOrderList.add(new Order(4,400,Currency.UAH,"ItemName4","Shop4", new User(40,"FirstName4", "LastName4", "Lviv", 4001)));
+
+        for (Order getCity : makeOrderList){
+            if (getCity.getUser().getCity() != null){
+                cities.add(getCity.getUser().getCity());
+            }
+        }
+        for (String s : cities){
+            List<Order> temp = new ArrayList<>();
+            for (int i = 0; i < makeOrderList.size(); i++){
+                if (makeOrderList.get(i).getUser().getCity() == s){
+                    temp.add(makeOrderList.get(i));
+                }
+            }
+            orderLists.add(temp);
+        }
+
+        System.out.println(cities);
+        System.out.println("as many lists as many unique cities are in User: "+ orderLists);
 
     //----------------------------------Homework 7.3 with users----------------------------------
         Set<Order> treeOrders = new TreeSet<>();
